dff6801715aa7b787a01b39bd8e6a304f8a5ea1f
Ihatelife11 <167127465+Ihatelife11@users.noreply.github.com>
2025-04-09 00:35:27 +0100

    Update 16714905773.lua

--- 16714905773.lua
+++ 16714905773.lua
@@ -16,18 +16,11 @@ local label = Instance.new("TextLabel", screenGui)
 label.Size = UDim2.new(1, 0, 1, 0)
 label.Position = UDim2.new(0, 0, 0, 0)
 label.BackgroundTransparency = 1
-label.TextColor3 = Color3.new(1, 1, 1) -- Pure white text
+label.TextColor3 = Color3.new(1, 1, 1) 
 label.Font = Enum.Font.GothamBold
 label.TextScaled = true
 label.Text = "Elysium is Paid"
 label.TextWrapped = true
-
-
-local sound = Instance.new("Sound", player:WaitForChild("PlayerGui"))
-sound.SoundId = "rbxassetid://9118823102" -- Static/Glitch sound
-sound.Volume = 1
-sound:Play()
-
 
 spawn(function()
 	while wait(0.07) do
